18f0eda13858f5a69fe6b4995ff986b777feec89
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-18 10:46:09 -0400

    Update Customer_lifetime.py

--- Customer_lifetime.py
+++ Customer_lifetime.py
@@ -1,7 +1,7 @@
 import json
 import altair as alt
 import pandas as pd
-import snowflake.snowpark as sp 
+
 from snowflake.snowpark import functions as F
 from snowflake.snowpark.session import Session
 from snowflake.snowpark import version as v
