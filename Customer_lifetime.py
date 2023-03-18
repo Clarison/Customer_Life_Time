a05a01529e2ad68fd154eb4f9391ccbc90e98032
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-18 11:00:59 -0400

    Update Customer_lifetime.py

--- Customer_lifetime.py
+++ Customer_lifetime.py
@@ -20,3 +20,53 @@ connection_parameters = {
   "database": "SNOWFLAKE_SAMPLE_DATA",
   "schema": "Public"
 }
+
+def create_session():
+    if "snowpark_session" not in st.session_state:
+        session = Session.builder.configs(connection_parameters).create()
+        st.session_state['snowpark_session'] = session
+    else:
+        session = st.session_state['snowpark_session']
+    return session
+
+
+
+st.text('')
+st.write(
+    f'<h1 style="font-size: 25px;">Customer Spending Prediction using Snowflake Dataplatform</h1>',
+    unsafe_allow_html=True
+)
+csv_file = st.file_uploader(label='Upload CSV file with user data', type = 'csv')
+
+def predict(inputs):
+
+    
+    Streamlit_data = session.table('Streamlit_data')
+    
+    snowdf_results = Streamlit_data.select(*inputs,
+                    call_udf("ml_xgboast_random_data",(*inputs)).alias('PREDICTION')
+                    )
+
+    return snowdf_results
+
+# Display the contents of the uploaded file as a DataFrame
+if csv_file is not None:
+    try:
+        df = pd.read_csv(csv_file)
+        st.write('Input data')
+        st.write(df)
+        
+        session = create_session()
+        session.use_warehouse('FE_AND_INFERENCE_WH')
+        session.use_database('tpcds_xgboost')
+        session.use_schema('demo')
+        df = session.create_dataframe(df)
+        df.write.mode('overwrite').save_as_table('Streamlit_data')
+        prediction = predict(df).to_pandas()
+        
+        st.write('Customer Spend Prediction')
+        st.write(prediction)
+        
+
+    except Exception as e:
+        st.write("Error reading CSV file:", e)
