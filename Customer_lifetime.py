3d2e09703219a7c60650661d311e2476827a7ff0
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-03-18 10:38:05 -0400

    Update Customer_lifetime.py

--- Customer_lifetime.py
+++ Customer_lifetime.py
@@ -1,2 +1,10 @@
 import json
-        
+import altair as alt
+import pandas as pd
+import snowflake.snowpark as sp 
+from snowflake.snowpark import functions as F
+from snowflake.snowpark.session import Session
+from snowflake.snowpark import version as v
+from snowflake.snowpark.functions import call_udf
+import streamlit as st
+       
